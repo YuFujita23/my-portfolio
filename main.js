d0e9117fe5761f6c7f4f6b07c5aee6bde85db627
YuFujita23 <a20.44h4@g.chuo-u.ac.jp>
2022-04-23 16:42:20 +0900

    ポートフォリオサイトの開発を進めました

--- main.js
+++ main.js
@@ -7,17 +7,17 @@ const button5 = document.getElementById("button5");
 const u = document.getElementById("u");
 
 button1.onclick = function () {
-  u.src = "wallpaper-8_3.jpg";
+  u.src = "/picture/wallpaper-8_3.jpg";
 };
 button2.onclick = function () {
-  u.src = "wallpaper-26_3.jpeg";
+  u.src = "/picture/wallpaper-26_3.jpeg";
 };
 button3.onclick = function () {
-  u.src = "wallpaper-27_3.jpeg";
+  u.src = "/picture/wallpaper-27_3.jpeg";
 };
 button4.onclick = function () {
-  u.src = "wallpaper-17_3.jpeg";
+  u.src = "/picture/wallpaper-17_3.jpeg";
 };
 button5.onclick = function () {
-  u.src = "wallpaper-13_1.jpeg";
+  u.src = "/picture/wallpaper-13_1.jpeg";
 };
